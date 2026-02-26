f59e23627f8cdd631fa07d80dd29880d15bc217e
Nahuel Perea <nahuel.perea.dev@gmail.com>
2026-02-25 23:16:55 -0300

    creacion de TransaccionRepo

---
+++ src/main/java/com/example/leerDatos/repository/TransaccionRepo.java
@@ -0,0 +1,10 @@
+package com.example.leerDatos.repository;
+
+import com.example.leerDatos.entitys.Transaccion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+
+public interface TransaccionRepo extends JpaRepository<Transaccion, UUID> {
+}
