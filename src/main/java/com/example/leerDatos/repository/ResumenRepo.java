43972c893b6c39437c0a753b728cf4719797873c
Nahuel Perea <nahuel.perea.dev@gmail.com>
2026-03-17 18:25:29 -0300

    cambios

--- src/main/java/com/example/leerDatos/repository/ResumenRepo.java
+++ src/main/java/com/example/leerDatos/repository/ResumenRepo.java
@@ -2,8 +2,9 @@ package com.example.leerDatos.repository;
 
 import com.example.leerDatos.entitys.Resumen;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
-
+@Repository
 public interface ResumenRepo extends JpaRepository<Resumen, UUID> {
 }
--- src/main/java/com/example/leerDatos/repository/TransaccionRepo.java
+++ src/main/java/com/example/leerDatos/repository/TransaccionRepo.java
@@ -2,11 +2,13 @@ package com.example.leerDatos.repository;
 
 import com.example.leerDatos.entitys.Transaccion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
 
+@Repository
 public interface TransaccionRepo extends JpaRepository<Transaccion, UUID> {
 
     List<Transaccion> findByCategoria(String categoria);
